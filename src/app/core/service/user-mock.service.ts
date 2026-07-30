72e4fe6b8c781cdf7a42db832310fa92ef40c7d5
pmjba <pmj.bailly@gmail.com>
2026-07-30 08:58:14 +0200

    Add Login model and login service method

--- src/app/core/service/user-mock.service.ts
+++ src/app/core/service/user-mock.service.ts
@@ -1,10 +1,15 @@
 import {Register} from '../models/Register';
+import {Login} from '../models/Login';
 import {Observable, of} from 'rxjs';
 
 
 export class UserMockService {
 
   register(user: Register): Observable<Object> {
-    return of();
+    return of(null);
+  }
+
+  login(login: Login): Observable<Object> {
+    return of({ token: 'mock-token' });
   }
 }
